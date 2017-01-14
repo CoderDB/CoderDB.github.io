0783e520b2f55e66d20f00fc231e0e0917d1a12c
redtwowolf <dongbinghouy@gmail.com>
2017-01-14 22:31:29 +0800

    update test.hs to pattern matching

--- _code/HaskellAndSwift/test.hs
+++ _code/HaskellAndSwift/test.hs
@@ -4,3 +4,21 @@ a = 5
 -- doubleUs x y = (x + y) * 2
 
 removeUppercase s = [x | x <- s, x `elem` ['a'..'z']]
+
+-- Type
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+circleArea :: Double -> Double
+circleArea r = pi * r * r
+-- circumference' r ^ 2 / (4 * pi)
+
+-- Pattern matching
+notAtWork :: String -> String
+notAtWork "Monday" = "Today is Monday, you ji ge mao a."
+notAtWork "Wednesday" = "You don't have to go to work after three days."
+notAtWork "Friday" = "You don't need to work tomorrow."
+notAtWork x = "You do have to work everyday, you konw, shabi!"
